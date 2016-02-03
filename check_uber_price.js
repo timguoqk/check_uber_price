85564a3d26c38cdbbee25a23243ca20e957439cd
Tim Guo <timguoqk@users.noreply.github.com>
2016-02-02 19:33:44 -0500

    format output

--- check_uber_price.js
+++ check_uber_price.js
@@ -1,8 +1,11 @@
-// Run `npm install request` before running this script
-var request = require('request');
 var querystring = require('querystring');
 
+var request = require('request');
+var _ = require('underscore');
+
 var server_token = 'X5Svtc45OVGhABAdzM-hHaqdKFUdoe9P5KSJT-Ih'; 
+// Services that you are interested
+var targets = ['uberX'];
 
 if (process.argv.length != 6) {
   console.log("Need four arguments!");
@@ -18,8 +21,21 @@ var query_cost = {
 };
 
 // Unsafe and hacky, to get rid of potential cert issues
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+// Get current time
+console.log(new Date());
 
 request('https://api.uber.com/v1/estimates/price?' + querystring.stringify(query_cost), function (error, response, body) {
-  console.log(body);
+  var out = '';
+  _.filter(JSON.parse(body)['prices'], function(p) {
+    return _.contains(targets, p.display_name);
+  })
+    .forEach(function(p) {
+      out += p.display_name;
+      out += ' Surge: X' + p.surge_multiplier;
+      out += ' Price: ' + p.estimate;
+      out += '\n';
+    });
+  console.log(out);
 });
